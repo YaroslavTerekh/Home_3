3899f89c5f991efae4176dcbeb5790ab2939242e
Yaroslav Terekh <yarolslavterekh@gmail.com>
2022-07-28 20:37:07 +0300

    CreateProject

---
+++ Models/Heros/Tor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HeroBattle.Services;
+
+namespace HeroBattle.Models.Heros
+{
+    public class Tor : BaseHero
+    {
+        public override int Armor { get; set; } = 6;
+
+        public override int Damage { get; set; } = 14;
+
+        public override string ToString()
+        {
+            return "Tor";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HeroBattle.Models;
+using HeroBattle.Models.Heros;
+using HeroBattle.Services.Implementations;
+
+namespace HeroBattle
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            BaseHero hero1 = BattleHelper.GenerateHero("Please select a hero. (1,2,3,4)");
+            BaseHero hero2 = BattleHelper.GenerateHero("Please select a second hero. (1,2,3,4)");
+
+            hero1.AddSuperPower(
+                new ArmorSuperPowerService(),
+                SuperPowerGenerator.GenerateSuperPower());
+
+            hero2.AddSuperPower(
+                new HPSuperPowerService(),
+                SuperPowerGenerator.GenerateSuperPower());
+
+            Battle.Fight(hero1, hero2);
+            Console.ReadKey();
+        }
+    }
+}
